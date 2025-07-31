c6ce8a2b93bfa13d09b81fd474bbfc545286d7ca
tibo1621 <thibaulddaix@gmail.com>
2025-07-31 23:29:50 +0200

    Update script_section.js

--- static/script_section.js
+++ static/script_section.js
@@ -1,22 +1,23 @@
 document.getElementById("btnCalcul").addEventListener("click", function () {
   // Récupérer les valeurs
-  const courant = parseFloat(document.getElementById("courant").value);
-  const temps = parseFloat(document.getElementById("temps").value);
+  const nb_courant = parseFloat(document.getElementById("nb_courant").value);
+  const tension = parseFloat(document.getElementById("tension").value);
+  const longueur = parseFloat(document.getElementById("longueur").value);
 
 
   // Vérification des valeurs
   if (
-    isNaN(courant) || isNaN(temps)
+    isNaN(nb_courant) || isNaN(tension) || isNaN(longueur)
   ) {
     alert("Veuillez remplir correctement tous les champs nécessaires.");
     return;
   }
 
   // Calculs intermédiaires
-  const capacite = courant * (temps/60);
+  const section = (2 * longueur * nb_courant * resistivite) / chute_tension;
 
   // Affichage résultats dans les inputs
-  document.getElementById("capacite").value = capacite.toFixed(2);
+  document.getElementById("section").value = section.toFixed(2);
 
   // Afficher la section résultats si cachée
   const sectionResu = document.getElementById("resu");
